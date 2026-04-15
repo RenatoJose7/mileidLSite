0a760dd51bf3724ba7af5c8cbff72657973d90f5
Renato <renatojosesfc@gmail.com>
2026-04-14 21:51:52 -0300

    .

---
+++ script.js
@@ -0,0 +1,6 @@
+function whatsapp(){
+  window.open(
+    "https://wa.me/5511921595542?text=Olá,%20vim%20pela%20página%20e%20gostaria%20de%20agendar%20uma%20avaliação%20psicopedagógica.",
+    "_blank"
+  );
+}
